887c096772d1bc8b3a8db06da579bc4efe5b6da8
Matheus Hora <matheus11hora@gmail.com>
2025-03-04 17:26:55 -0300

    ✨ feat: Cria a exception

---
+++ app_architecture_flutter/lib/data/exceptions/exceptions.dart
@@ -0,0 +1,6 @@
+import 'package:app_architecture_flutter/utils/exceptions/exceptions.dart';
+
+class LocalStorageException extends AppExceptions {
+  LocalStorageException(super.message, [super.stackTrace]);
+}
+
