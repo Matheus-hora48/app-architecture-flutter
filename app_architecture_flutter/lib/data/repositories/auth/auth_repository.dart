e4d5999c04c92d1f3c899121a59acbbebc59f536
Matheus Hora <matheus11hora@gmail.com>
2025-03-04 17:22:30 -0300

    ✨ feat: Cria o repository de auth

--- app_architecture_flutter/lib/data/repositories/auth/auth_repository.dart
+++ app_architecture_flutter/lib/data/repositories/auth/auth_repository.dart
@@ -1,9 +1,11 @@
+import 'package:app_architecture_flutter/domain/dtos/credentials.dart';
 import 'package:app_architecture_flutter/domain/entities/user_entity.dart';
 import 'package:result_dart/result_dart.dart';
 
 abstract interface class AuthRepository {
-  AsyncResult<LoggedUser> login();
+  AsyncResult<LoggedUser> login(Credentials credentials);
   AsyncResult<Unit> logout();
   AsyncResult<LoggedUser> getUser();
   Stream<User> userObserver();
+  void dispose();
 }
--- app_architecture_flutter/lib/data/repositories/auth/remote_auth_repository.dart
+++ app_architecture_flutter/lib/data/repositories/auth/remote_auth_repository.dart
@@ -1,32 +1,54 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'dart:async';
 
-import 'package:app_architecture_flutter/data/repositories/auth/auth_repository.dart';
-import 'package:app_architecture_flutter/domain/entities/user_entity.dart';
+import 'package:app_architecture_flutter/data/services/auth/auth_client_http.dart';
+import 'package:app_architecture_flutter/domain/dtos/credentials.dart';
+import 'package:app_architecture_flutter/domain/validators/credentials_validator.dart';
+import 'package:app_architecture_flutter/utils/validation/lucid_validator_extension.dart';
 import 'package:result_dart/result_dart.dart';
 
-class RemoteAuthRepository implements AuthRepository{
+import 'package:app_architecture_flutter/data/repositories/auth/auth_repository.dart';
+import 'package:app_architecture_flutter/data/services/auth/auth_local_storage.dart';
+import 'package:app_architecture_flutter/domain/entities/user_entity.dart';
+
+class RemoteAuthRepository implements AuthRepository {
+  RemoteAuthRepository(this._authLocalStorage, this._authClientHttp);
+  final _streamController = StreamController<User>.broadcast();
+
+  final AuthLocalStorage _authLocalStorage;
+  final AuthClientHttp _authClientHttp;
+
   @override
   AsyncResult<LoggedUser> getUser() {
-    // TODO: implement getUser
-    throw UnimplementedError();
+    return _authLocalStorage.getUser();
   }
 
   @override
-  AsyncResult<LoggedUser> login() {
-    // TODO: implement login
-    throw UnimplementedError();
+  AsyncResult<LoggedUser> login(Credentials credentials) async {
+    final validator = CredentialsValidator();
+
+    return validator 
+        .validateResult(credentials)
+        .flatMap(_authClientHttp.login)
+        .flatMap(_authLocalStorage.saveUser)
+        .onSuccess(_streamController.add);
   }
 
   @override
   AsyncResult<Unit> logout() {
-    // TODO: implement logout
-    throw UnimplementedError();
+    return _authLocalStorage
+        .removeUser()
+        .onSuccess((_) => _streamController.add(const NotLoggedUser()));
   }
 
   @override
   Stream<User> userObserver() {
-    // TODO: implement userObserver
-    throw UnimplementedError();
+    return _streamController.stream;
   }
-  
+
+  @override
+  void dispose() {
+    _streamController.close();
+  }
 }
